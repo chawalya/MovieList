fcfc2ea9d8487cf00a6b584e005c549aa73f6c6a
chawalya tantisevi <chawalyaa@gmail.com>
2019-09-02 17:48:38 +0700

    first commit

---
+++ MovieList/Scences/Main/Cell/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  MovieList
+//
+//  Created by Chawalya Tantisevi on 2/9/2562 BE.
+//  Copyright © 2562 SCB. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
